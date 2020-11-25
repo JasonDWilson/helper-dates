a67bc04d1ae604b2c64de01731f93646857e8873
Jason Wilson <jason.wilson@radpartners.com>
2020-11-25 14:51:52 -0600

    Added date extension methods to test for each special date

--- helper-dates-tests/DateExtensionsTest.cs
+++ helper-dates-tests/DateExtensionsTest.cs
@@ -12,6 +12,201 @@ namespace helper_dates_tests
     public class DateExtensionsTest
     {
         [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("12/25/2020", true)]
+        public void IsChristmasDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsChristmasDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("12/24/2020", true)]
+        public void IsChristmasEveTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsChristmasEve();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("10/12/2020", true)]
+        public void IsColumbusDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsColumbusDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("7/4/2020", true)]
+        public void IsIndependenceTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsIndependenceDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("9/7/2020", true)]
+        public void IsLaborDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsLaborDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("1/20/2020", true)]
+        public void IsMartinLutherKingJrDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsMartinLutherKingJrDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("5/25/2020", true)]
+        public void IsMemorialDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsMemorialDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("1/1/2020", true)]
+        public void IsNewYearsDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsNewYearsDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("12/31/2020", true)]
+        public void IsNewYearsEveTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsNewYearsEve();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("2/17/2020", true)]
+        public void IsPresidentsDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsPresidentsDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("11/27/2020", true)]
+        public void IsThanksgivingDayAfterTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsThanksgivingDayAfter();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("11/26/2020", true)]
+        public void IsThanksgivingDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsThanksgivingDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("11/11/2020", true)]
+        public void IsVeteransDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = inputDate.IsVeteransDay();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
         [InlineData("11/25/2020", "Christmas_Eve", false)]
         [InlineData("12/24/2020", "Christmas_Eve", true)]
         [InlineData("11/25/2020", "Christmas_Day", false)]
--- helper-dates/Extensions/DateExtensions.cs
+++ helper-dates/Extensions/DateExtensions.cs
@@ -10,6 +10,34 @@ namespace jwpro.DateHelper.Extensions
 {
     public static class DateExtensions
     {
+        public static bool IsChristmasDay(this DateTime input) => SpecialDateHelper.IsChristmasDay(input);
+
+        public static bool IsChristmasEve(this DateTime input) => SpecialDateHelper.IsChristmasEve(input);
+
+        public static bool IsColumbusDay(this DateTime input) => SpecialDateHelper.IsColumbusDay(input);
+
+        public static bool IsIndependenceDay(this DateTime input) => SpecialDateHelper.IsIndependenceDay(input);
+
+        public static bool IsLaborDay(this DateTime input) => SpecialDateHelper.IsLaborDay(input);
+
+        public static bool IsMartinLutherKingJrDay(this DateTime input) => SpecialDateHelper.IsMartinLutherKingJrDay(
+            input);
+
+        public static bool IsMemorialDay(this DateTime input) => SpecialDateHelper.IsMemorialDay(input);
+
+        public static bool IsNewYearsDay(this DateTime input) => SpecialDateHelper.IsNewYearsDay(input);
+
+        public static bool IsNewYearsEve(this DateTime input) => SpecialDateHelper.IsNewYearsEve(input);
+
+        public static bool IsPresidentsDay(this DateTime input) => SpecialDateHelper.IsPresidentsDay(input);
+
+        public static bool IsThanksgivingDay(this DateTime input) => SpecialDateHelper.IsThanksgivingDay(input);
+
+        public static bool IsThanksgivingDayAfter(this DateTime input) => SpecialDateHelper.IsThanksgivingDayAfter(
+            input);
+
+        public static bool IsVeteransDay(this DateTime input) => SpecialDateHelper.IsVeteransDay(input);
+
         public static IEnumerable<SpecialDates> SpecialDates(this DateTime input)
         {
             List<SpecialDates> result = new List<SpecialDates>();
