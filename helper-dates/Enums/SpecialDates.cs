24f3f546db4e5991a46d170814ac3a9b8500b136
Jason Wilson <jason.wilson@radpartners.com>
2020-11-25 13:27:23 -0600

    Added special dates enum

---
+++ helper-dates/Enums/SpecialDates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jwpro.DateHelper.Enums
+{
+    public enum SpecialDates
+    {
+        Christmas_Eve,
+        Christmas_Day,
+        Columbus_Day,
+        Independence_Day,
+        Labor_Day,
+        Martin_Luther_King_Jr_Day,
+        Memorial_Day,
+        New_Years_Day,
+        New_Years_Eve,
+        Presidents_Day,
+        Thanksgiving_Day,
+        Thanksgiving_Day_After,
+        Veterans_Day,
+    }
+}
