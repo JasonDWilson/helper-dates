afb64329dfb0a0a00281cc42ec33aff29dff6705
Jason Wilson <jason.wilson@radpartners.com>
2020-11-25 13:29:25 -0600

    dated a class to hold date extenstions

---
+++ helper-dates/Extensions/DateExtensions.cs
@@ -0,0 +1,21 @@
+﻿using jwpro.DateHelper.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jwpro.DateHelper.Extensions
+{
+    public static class DateExtensions
+    {
+        public static IEnumerable<SpecialDates> SpecialDates(this DateTime input)
+        {
+            List<SpecialDates> result = new List<SpecialDates>();
+            foreach(SpecialDates special in Enum.GetValues(typeof(SpecialDates)))
+            {
+            }
+            return result;
+        }
+    }
+}
