6462fafdc14082d050685e90c69bae2bb3b052a5
Jason Wilson <jason.wilson@radpartners.com>
2020-11-25 13:28:18 -0600

    added tests for the special date helper

---
+++ helper-dates-tests/SpecialDateHelperTests.cs
@@ -0,0 +1,272 @@
+﻿using jwpro.DateHelper.Enums;
+using jwpro.DateHelper.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace helper_dates_tests
+{
+    public class SpecialDateHelperTests
+    {
+        [Fact]
+        public void GetChristmasDayTest()
+        {
+            // arrange
+            var expected = DateTime.Parse("12/25/2020");
+            // act
+            var actual = SpecialDateHelper.GetChristmasDay("2020");
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetChristmasEveTest()
+        {
+            // arrange
+            var expected = DateTime.Parse("12/24/2020");
+            // act
+            var actual = SpecialDateHelper.GetChristmasEve("2020");
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetColumbusDayTest()
+        {
+            // arrange
+            var expected = DateTime.Parse("10/12/2020");
+            // act
+            var actual = SpecialDateHelper.GetColumbusDay("2020");
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetIndependenceDayTest()
+        {
+            // arrange
+            var expected = DateTime.Parse("7/4/2020");
+            // act
+            var actual = SpecialDateHelper.GetIndependenceDay("2020");
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetLaborDayTest()
+        {
+            // arrange
+            var expected = DateTime.Parse("9/7/2020");
+            // act
+            var actual = SpecialDateHelper.GetLaborDay("2020");
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetMartinLutherKingJrDayTest()
+        {
+            // arrange
+            var expected = DateTime.Parse("1/20/2020");
+            // act
+            var actual = SpecialDateHelper.GetMartinLutherKingJrDay("2020");
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetMemorialDayTest()
+        {
+            // arrange
+            var expected = DateTime.Parse("5/25/2020");
+            // act
+            var actual = SpecialDateHelper.GetMemorialDay("2020");
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetNewYearsDayTest()
+        {
+            // arrange
+            var expected = DateTime.Parse("1/1/2020");
+            // act
+            var actual = SpecialDateHelper.GetNewYearsDay("2020");
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetNewYearsEveTest()
+        {
+            // arrange
+            var expected = DateTime.Parse("12/31/2020");
+            // act
+            var actual = SpecialDateHelper.GetNewYearsEve("2020");
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Theory]
+        [InlineData("12/25/2020", "2020", SpecialDates.Christmas_Day)]
+        [InlineData("12/24/2020", "2020", SpecialDates.Christmas_Eve)]
+        [InlineData("10/12/2020", "2020", SpecialDates.Columbus_Day)]
+        [InlineData("7/4/2020", "2020", SpecialDates.Independence_Day)]
+        [InlineData("9/7/2020", "2020", SpecialDates.Labor_Day)]
+        [InlineData("1/20/2020", "2020", SpecialDates.Martin_Luther_King_Jr_Day)]
+        [InlineData("5/25/2020", "2020", SpecialDates.Memorial_Day)]
+        [InlineData("1/1/2020", "2020", SpecialDates.New_Years_Day)]
+        [InlineData("12/31/2020", "2020", SpecialDates.New_Years_Eve)]
+        public void GetSpecialDateTest(string inputExpected, string inputYear, SpecialDates special)
+        {
+            // arrange
+            var expected = DateTime.Parse(inputExpected);
+
+            // act
+            var actual = SpecialDateHelper.GetSpecialDate(special, inputYear);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("12/25/2020", true)]
+        public void IsChristmasDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = SpecialDateHelper.IsChristmasDay(inputDate);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("12/24/2020", true)]
+        public void IsChristmasEveTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = SpecialDateHelper.IsChristmasEve(inputDate);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("10/12/2020", true)]
+        public void IsColumbusDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = SpecialDateHelper.IsColumbusDay(inputDate);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("7/4/2020", true)]
+        public void IsIndependenceTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = SpecialDateHelper.IsIndependenceDay(inputDate);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("9/7/2020", true)]
+        public void IsLaborDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = SpecialDateHelper.IsLaborDay(inputDate);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("1/20/2020", true)]
+        public void IsMartinLutherKingJrDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = SpecialDateHelper.IsMartinLutherKingJrDay(inputDate);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("5/25/2020", true)]
+        public void IsMemorialDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = SpecialDateHelper.IsMemorialDay(inputDate);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("1/1/2020", true)]
+        public void IsNewYearsDayTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = SpecialDateHelper.IsNewYearsDay(inputDate);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("11/25/2020", false)]
+        [InlineData("12/31/2020", true)]
+        public void IsNewYearsEveTest(string input, bool expected)
+        {
+            // arrange
+            var inputDate = DateTime.Parse(input);
+
+            // act
+            var actual = SpecialDateHelper.IsNewYearsEve(inputDate);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
+
