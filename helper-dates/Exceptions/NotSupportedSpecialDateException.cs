f5085860027c13bd8df2747c708122257de51f61
Jason Wilson <jason.wilson@radpartners.com>
2020-11-25 13:28:48 -0600

    added a custom exception for unsupported special dates

---
+++ helper-dates/Exceptions/NotSupportedSpecialDateException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jwpro.DateHelper.Exceptions
+{
+    public class NotSupportedSpecialDateException : Exception
+    {
+        public NotSupportedSpecialDateException()
+        {
+        }
+
+        public NotSupportedSpecialDateException(string message) : base(message)
+        {
+        }
+
+        public NotSupportedSpecialDateException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
