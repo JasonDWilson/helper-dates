feb0c3035ffa1ceb2b03016fab43ee9fdfb99c87
Jason Wilson <jason.wilson@radpartners.com>
2020-11-26 10:02:54 -0600

    Added BusinessDateManager and BusinessDateManagerConfiguration

---
+++ helper-dates/Managers/BusinesDateManager.cs
@@ -0,0 +1,17 @@
+﻿using jwpro.DateHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jwpro.DateHelper.Managers
+{
+    public class BusinesDateManager
+    {
+        BusinessDateManagerConfiguration _config;
+
+        public BusinesDateManager(BusinessDateManagerConfiguration config)
+        { _config = config ?? throw new ArgumentNullException(nameof(config)); }
+    }
+}
