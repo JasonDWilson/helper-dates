1b513d2636eeb3c0e7da5533c0fcab1cb80b4f2a
Jason Wilson <jason.wilson@radpartners.com>
2020-11-28 09:10:22 -0600

    Added BusinessDayBegn and BusinessDayEnd to BusinessDateManagerConfiguration

--- helper-dates/Configuration/BusinessDateManagerConfiguration.cs
+++ helper-dates/Configuration/BusinessDateManagerConfiguration.cs
@@ -3,12 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace jwpro.DateHelper.Configuration
 {
     public class BusinessDateManagerConfiguration
     {
+        private string _businessDayBegin;
+        private string _businessDayEnd;
+
+        private bool IsMilitaryTime(string time) { return Regex.IsMatch(time, @"([01]\d | 2[0 - 3])[0 - 5]\d"); }
+
+        public string BusinessDayBegin
+        {
+            get => _businessDayBegin;
+            set
+            {
+                if(!string.IsNullOrWhiteSpace(value) && !IsMilitaryTime(value))
+                    throw new ArgumentException("BusinessDayBegin must be a string in military time");
+                _businessDayBegin = value;
+            }
+        }
+
+        public string BusinessDayEnd
+        {
+            get => _businessDayEnd;
+            set
+            {
+                if(!string.IsNullOrWhiteSpace(value) && !IsMilitaryTime(value))
+                    throw new ArgumentException("BusinessDayEnd must be a string in military time");
+                _businessDayEnd = value;
+            }
+        }
+
         public List<PaidHoliday> PaidHolidays { get; set; } = new List<PaidHoliday>();
     }
 }
